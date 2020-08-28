19f6c6bbae185edce4b02cf109569263c9425475
TobiOladoja <tbfullstack@gmail.com>
2020-08-28 18:24:54 +0100

    Get City API Call

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,1 +1,15 @@
 const key = 'H7LRZ8nMsJVGInTwk9eQnMXD3ehinUar';
+
+const getCity = async (city) => {
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity('manchester')
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
